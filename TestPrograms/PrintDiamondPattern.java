68443075af9dc61448414e5ece2b8871de525ba3
Mounika-58 <24mca58@kgisliim.ac.in>
2025-05-07 15:21:07 +0530

    Update PrintDiamondPattern.java

--- TestPrograms/PrintDiamondPattern.java
+++ TestPrograms/PrintDiamondPattern.java
@@ -1,39 +1,44 @@
 package TestPrograms;
 
+import java.util.Scanner;
+
 public class PrintDiamondPattern {
 	
 	public static void main(String[] args) {
-		        int n = 5; // height of half of the diamond
+	
+		    	Scanner sc = new Scanner(System.in);
+		    	
+		    	System.out.print("n = ");
+		        int n = sc.nextInt(); 
 
-		        // Upper part of diamond
-		        for (int i = 1; i <= n; i++) {
-		            // Print leading spaces
-		            for (int j = i; j < n; j++) {
+		        int mid = (n + 1) / 2; 
+
+		        
+		        for (int i = 1; i <= mid; i++) {
+		            
+		            for (int j = i; j < mid; j++) {
 		                System.out.print(" ");
 		            }
-		            // Print stars
-		            for (int j = 1; j <= (2*i - 1); j++) {
-		                System.out.print("*");
+		            
+		            for (int k = 1; k <= i; k++) {
+		                System.out.print("* ");
 		            }
 		            System.out.println();
 		        }
 
-		        // Lower part of diamond
-		        for (int i = n - 1; i >= 1; i--) {
-		            // Print leading spaces
-		            for (int j = n; j > i; j--) {
+		      
+		        for (int i = mid - 1; i >= 1; i--) {
+		            // Print spaces
+		            for (int j = mid; j > i; j--) {
 		                System.out.print(" ");
 		            }
-		            // Print stars
-		            for (int j = 1; j <= (2*i - 1); j++) {
-		                System.out.print("*");
+		            
+		            for (int k = 1; k <= i; k++) {
+		                System.out.print("* ");
 		            }
 		            System.out.println();
 		        }
-		    
+		    }
 		}
 
-		
-		
-	
-}
+		       
